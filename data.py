c11cd3428e3430225bc5b59b793357e73169da7c
lyjdwh <1412511544@qq.com>
2020-06-21 06:41:54 +0800

    modify data getter method

--- data.py
+++ data.py
@@ -1,26 +1,28 @@
-# 38 x 68
-# 1. 预处理: 降采样 192x168 -> 48x42,  一维向量
-# 2. 分割数据集: 每类取 p 作为训练样本
-# 3. 实验
-#    1. 5 fold 参数选择 稀疏: p=7, 13, 20  字典大小: 200, 400, 600
-# TODO 每个文件夹有一张特别大的图，名字和其他图片不一样
+"""
+38 x 68
+1. 预处理: 降采样 192x168 -> 48x42,  一维向量
+2. 分割数据集: 每类取 p 作为训练样本
+3. 实验
+   1. 5 fold 参数选择 稀疏: p=7, 13, 20  字典大小: 200, 400, 600
+"""
 import glob
 
 from PIL import Image
 
 import numpy as np
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import RepeatedKFold, train_test_split
 
 
 class Data(object):
-    def __init__(self, fold, p):
+    def __init__(self, fold=5, p=7, n_repeats=1):
         self.fold = fold
         self.p = p  # 7, 13, 70
+        self.n_repeats = n_repeats
         self.data = dict()
         self.labels = np.zeros(2470, dtype=np.int16)
         self.features = np.zeros((2470, 2016))
-        self.test_x = None
-        self.test_y = None
+        self.test_x_ = None
+        self.test_y_ = None
 
     def preprocess(self):
         """
@@ -44,14 +46,18 @@ class Data(object):
         assert index == 2470
 
     def get_val_test_data(self):
-        train_x, self.test_x, train_y, self.test_y = train_test_split(
+        train_x, self.test_x_, train_y, self.test_y_ = train_test_split(
             self.features, self.labels, train_size=self.p * 38, shuffle=True
         )
-        kf = KFold(n_splits=self.fold)
+        # kf = KFold(n_splits=self.fold)
+        kf = RepeatedKFold(n_splits=self.fold, n_repeats=self.n_repeats)
         for val_index, test_index in kf.split(train_x):
             train_x, val_x = train_x[val_index], train_x[test_index]
             train_y, val_y = train_y[val_index], train_y[test_index]
-            yield train_x.T, val_x.T, train_y, val_y
+            yield train_x, val_x, train_y, val_y
 
     def get_test_data(self):
-        return self.test_x.T, self.test_y
+        return self.test_x_, self.test_y_
+
+    def get_data(self):
+        return self.features, self.labels
