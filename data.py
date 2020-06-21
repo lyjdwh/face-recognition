3c1c343349fb01778eec667db5428429d1c95f31
lyjdwh <1412511544@qq.com>
2020-06-21 11:39:54 +0800

    recontruct data gettter

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 """
-38 x 68
+38 x 65
 1. 预处理: 降采样 192x168 -> 48x42,  一维向量
 2. 分割数据集: 每类取 p 作为训练样本
 3. 实验
    1. 5 fold 参数选择 稀疏: p=7, 13, 20  字典大小: 200, 400, 600
 """
 import glob
+from random import shuffle
 
 from PIL import Image
 
@@ -18,11 +19,10 @@ class Data(object):
         self.fold = fold
         self.p = p  # 7, 13, 70
         self.n_repeats = n_repeats
-        self.data = dict()
-        self.labels = np.zeros(2470, dtype=np.int16)
-        self.features = np.zeros((2470, 2016))
-        self.test_x_ = None
-        self.test_y_ = None
+        self.train_x_ = list()
+        self.train_y_ = list()
+        self.test_x_ = list()
+        self.test_y_ = list()
 
     def preprocess(self):
         """
@@ -31,33 +31,48 @@ class Data(object):
         face_pathes = glob.glob(r"data/*")
         face_names = [path.split("/")[1] for path in face_pathes]
 
-        index = 0
         for face_name in face_names:
             faces = glob.glob("data/%s/*.pgm" % face_name)
-            face_class = face_name[-2:]
-            self.labels[index] = int(face_class)
+
+            shuffle(faces)
+            face_class = int(face_name[-2:])
+            index = 0
             for face in faces:
                 image = Image.open(face)
                 image = image.resize((48, 42))
-                image = np.asarray(image)
-                self.features[index, :] = image.flatten()
+                image = np.asarray(image).flatten()
+                if index < self.p:
+                    self.train_x_.append(image)
+                    self.train_y_.append(face_class)
+                else:
+                    self.test_x_.append(image)
+                    self.test_y_.append(face_class)
                 index += 1
+        self.train_x_ = np.asarray(self.train_x_)
+        self.train_y_ = np.asarray(self.train_y_)
 
-        assert index == 2470
+        # shuffle
+        self.train_x_, _, self.train_y_, _ = train_test_split(
+            self.train_x_, self.train_y_, test_size=1e-10, shuffle=True
+        )
+
+        self.test_x_ = np.asarray(self.test_x_)
+        self.test_y_ = np.asarray(self.test_y_)
+        # shuffle
+        self.test_x_, _, self.test_y_, _ = train_test_split(
+            self.test_x_, self.test_y_, test_size=1e-10, shuffle=True
+        )
 
     def get_val_test_data(self):
-        train_x, self.test_x_, train_y, self.test_y_ = train_test_split(
-            self.features, self.labels, train_size=self.p * 38, shuffle=True
-        )
         # kf = KFold(n_splits=self.fold)
         kf = RepeatedKFold(n_splits=self.fold, n_repeats=self.n_repeats)
-        for val_index, test_index in kf.split(train_x):
-            train_x, val_x = train_x[val_index], train_x[test_index]
-            train_y, val_y = train_y[val_index], train_y[test_index]
+        for val_index, test_index in kf.split(self.train_x_):
+            train_x, val_x = self.train_x_[val_index], self.train_x_[test_index]
+            train_y, val_y = self.train_y_[val_index], self.train_y_[test_index]
             yield train_x, val_x, train_y, val_y
 
     def get_test_data(self):
         return self.test_x_, self.test_y_
 
-    def get_data(self):
-        return self.features, self.labels
+    def get_train_data(self):
+        return self.train_x_, self.train_y_
